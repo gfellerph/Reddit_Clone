10d2da44c868e29449814fd926fddfacbdcb8a92
tuelsch <tuelsch@gmail.com>
2014-06-03 09:08:28 +0200

    minor update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-define([
-	"vendor/modernizr",
-	"modules/post",
-	"modules/nav"
-]);
+/*
+	Main JS file for require.js
+	===========================
+
+	Imports jquery and bootstrap js from CDN's with a local
+	fallback.
+*/
+
+require.config({
+	paths: {
+		'jquery': [ // CDN jQuery with local fallback
+			'//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+			'vendor/jquery.min'
+		],
+		'bootstrap': [ // CDN Bootstrap with local fallback
+			'//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min',
+			'vendor/bootstrap.min'
+		]
+	},
+	shim: { // Shim for external scripts
+		'bootstrap': {
+			deps: ['jquery'] // Bootstrap depends on jquery
+		}
+	}
+});
+
+require([
+	'vendor/modernizr',
+	'bootstrap',
+	'modules/post',
+	'modules/nav'
+	/* more modules */
+	
+], function(Modernizr, module){
+
+});
