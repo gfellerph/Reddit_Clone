4c91a71a2720b4978d53f6b4142337ceb13d256b
Philipp Gfeller <tuelsch@gmail.com>
2014-07-05 10:35:52 +0200

    up

--- assets/js/modules/nav.js
+++ assets/js/modules/nav.js
@@ -3,26 +3,44 @@ var jQuery = require('../vendor/jquery');
 +function navigation($){
 
 	// Live event binding
-	$(document).on('click', '#menu [data-folder]', folder_click);
+	$(document).on('click', '[data-expands]', expand);
 
 	// Handler for the click event on a menu point with a folder
-	function folder_click(event){
-
+	function expand(event){
+		console.log('expand');
 		// Get the clicked menu point and all folders
 		var $target = $(event.currentTarget),
-			$folder = $('#menu .folder');
+			$expander = $('#expander');
 
-		if($folder.hasClass('open')){
-			$folder.css({'height': 0});
-			$folder.removeClass('open');
+		// Look for open panels
+		if($expander.hasClass('open')){
+			// If there are any, fade them out and in the callback, fade the new one in
+			$expander.data('openPanel').fadeOut(
+				200, 
+				showPanel($target.attr('data-expands'))
+			);
 		} else {
-			var $section = $folder.find('.' + $target.attr('data-folder'));
-
-			$folder.find('section').hide();
-			$target.show();
-
-			$folder.css({'height': $target.outerHeight()});
-			$folder.addClass('open');
+			// If there are none, fade in new panel
+			showPanel($target.attr('data-expands'));
 		}
+
+		function showPanel(ident){
+			var $panel = $expander.find('[data-expandable="' + ident + '"]');
+
+			$expander.css({'height': $panel.outerHeight()});
+			$expander.addClass('open');
+			$panel.fadeIn(200);
+			$expander.data('openPanel', $panel);
+		}
+	}
+
+
+	// Activate the clicked filter
+	var $filters = $('#header .filter');
+	$filters.on('click', filter_click);
+	function filter_click(e){
+		e.preventDefault();
+		$filters.removeClass('active');
+		$(e.currentTarget).addClass('active');
 	}
 }(jQuery);
