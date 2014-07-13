4f2b5948320b943111298427ba468544d1f6d445
tuelsch <tuelsch@gmail.com>
2014-07-13 17:26:30 +0200

    Included Comment Script (main.js)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,4 @@ require('./modules/expander'); 			// Show/hide the menu panels
 require('./modules/reddit-api-mantle'); // Get data from reddit
 require('./modules/add-post');			// Add a post with the form
 require('./modules/voting');			// The voting logic
+require('./modules/comments');			// Comment logic
