1ef29637512d13ebf9612f930b1eccab2b784334
tuelsch <tuelsch@gmail.com>
2014-07-13 17:42:34 +0200

    Better voting function

--- assets/js/modules/voting.js
+++ assets/js/modules/voting.js
@@ -5,33 +5,59 @@
 
 	function upvote(e){
 
+		e.preventDefault();
+
 		// Get the data object (post) from the parent <li>
 		var $target = $(e.currentTarget);
 		var $post = $target.parents('.post');
 		var $score = $post.find('.score');
 		var postObject = $post.data('post');
+		var n = 1;
 
-		postObject.score += 1;
+		// If already downvoted, correct downvote and upvote
+		if(postObject.downvoted){
+			n = 2;
+			postObject.downs -= 1;
+			postObject.downvoted = false;
+		}
+
+		// If already upvoted, leave
+		if(postObject.upvoted) return false;
+
+		postObject.upvoted = true;
+		postObject.score += n;
 		postObject.ups += 1;
 		$score.html(postObject.score);
 
-		e.preventDefault();
 		return false;
 	}
 
 	function downvote(e){
+
+		e.preventDefault();
 		
 		// Get the data object (post) from the parent <li>
 		var $target = $(e.currentTarget);
 		var $post = $target.parents('.post');
 		var $score = $post.find('.score');
 		var postObject = $post.data('post');
+		var n = -1;
 
-		postObject.score -= 1;
-		postObject.downs -= 1;
+		// If already upvoted, correct upvote and downvote
+		if(postObject.upvoted){
+			n = -2;
+			postObject.ups -= 1;
+			postObject.upvoted = false;
+		}
+
+		// If already downvoted, leave
+		if(postObject.downvoted) return false;
+
+		postObject.downvoted = true;
+		postObject.score += n;
+		postObject.downs += 1;
 		$score.html(postObject.score);
 
-		e.preventDefault();
 		return false;
 	}
 }(require('../vendor/jquery'));
