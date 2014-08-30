0e2beb0eb9c09a16cb5f4ff5f6b940bb6c3e372b
Philipp Gfeller <tuelsch@gmail.com>
2014-08-30 10:21:27 +0200

    Updated File Structure

---
+++ frontend/buildsystem/tasks/iconfont.js
@@ -0,0 +1,85 @@
+// Dependencies
+// ============
+
+var gulp 		= require('gulp');
+var plumber 	= require('gulp-plumber');
+var iconfont 	= require('gulp-iconfont');
+var gutil		= require('gulp-util');
+var consolidate = require('gulp-consolidate');
+var rename 		= require('gulp-rename');
+
+
+// Options
+// =======
+
+var options = {
+
+	// Where you want to store the svg Files
+	svgBaseDir: '../svg/**/*.svg',
+
+	// Where the font files will be saved to
+	iconfontOutputDir: '../../assets/fonts/',
+
+	// Name to use on generated webfont, in the less file and as font-family name		
+	fontName: 'fonticons',
+
+	// Class name to use in less files, name of the mixin
+	className: 'gfx',
+
+	// Path to the less template
+	lessTemplate: 'tasks/iconfont-template.less',
+
+	// Where the generated less file should be saved to
+	lessOutputFilePath: '../less/code/',
+
+	// This path will be written in the @fontface rule in the less file
+	fontServerPath: '/assets/fonts/'
+}
+
+
+// Tasks
+// =====
+
+gulp.task('iconfont', buildIconfont);
+
+
+// Watcher
+// =======
+
+gulp.watch(options.svgBaseDir).on('change', buildIconfont);
+
+
+// Functions
+// =========
+
+function buildIconfont(){
+
+	gulp.src(options.svgBaseDir)
+	.pipe( plumber() )
+	.pipe( iconfont({
+		fontName: options.fontName,
+		//fixedWidth: false,
+		//centerHorizontally: false,
+		normalize: true,
+		//fontHeight: 512,
+		//descent: 0,
+		appendCodepoints: true
+	}))
+	.on('codepoints', function(codepoints){
+		gulp.src(options.lessTemplate)
+		.pipe( plumber() )
+		.pipe( consolidate('lodash', {
+			glyphs: codepoints,
+			fontName: options.fontName,
+			fontPath: options.fontServerPath,
+			className: options.className
+		}))
+		.pipe( rename(options.fontName + '.less') )
+		.pipe( gulp.dest(options.lessOutputFilePath) );
+
+		gutil.log('Built less fontfile');
+	})
+	.pipe(gulp.dest(options.iconfontOutputDir));
+
+	gutil.log('Built iconfont');
+}
---
+++ frontend/buildsystem/tasks/less.js
@@ -0,0 +1,69 @@
+// Dependencies
+// ============
+
+var gulp 		= require('gulp');
+var plumber 	= require('gulp-plumber');
+var less 		= require('gulp-less');
+var rename 		= require('gulp-rename');
+var minify		= require('gulp-minify-css');
+var sourcemaps 	= require('gulp-sourcemaps');
+var gutil		= require('gulp-util');
+
+
+// Options
+// =====
+
+var options = {
+	lessBaseDir: '../less/**/*.less', 		// Where all less files are stored and the watcher is set
+	lessMainFile: '../less/main.less', 	// Where your less files are imported
+	lessOutputDir: '../../assets/css', 			// Where main.css, main.min.css and the sourcemap would be written to
+	lessOutputMinName: 'main.min.css'	// Name of the minified css
+}
+
+
+// Tasks
+// =====
+
+gulp.task('less', lessbuild);
+
+
+// Watcher
+// =======
+
+gulp.watch(options.lessBaseDir).on('change', lessbuild);
+
+
+// Functions
+// =========
+
+function lessbuild(event){
+
+	// Start piping with main file
+	gulp.src(options.lessMainFile)
+
+	// Report errors and keep watching
+	.pipe( plumber() )
+
+	// Initialize sourcemaps
+	.pipe( sourcemaps.init() )
+
+	// Compile less and generate sourcemap
+	.pipe( less() )
+
+	// Generate sourcemap
+	.pipe( sourcemaps.write() )
+
+	// Save compiled css
+	.pipe( gulp.dest(options.lessOutputDir) )
+
+	// Rename stream
+	.pipe( rename(options.lessOutputMinName) )
+
+	// Minify
+	.pipe( minify() )
+
+	// Save minified file
+	.pipe( gulp.dest(options.lessOutputDir) );
+
+	gutil.log('Less built');
+}
